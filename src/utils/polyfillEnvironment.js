2d8ff253117363311a1b3ff550f91aa282fdbeb2
spypsy <spypsy@users.noreply.github.com>
2017-05-05 17:40:44 +0100

    Update polyfillEnvironment.js

--- src/utils/polyfillEnvironment.js
+++ src/utils/polyfillEnvironment.js
@@ -28,10 +28,10 @@ require('../../vendor/polyfills/babelHelpers.js');
 //   check to fail.  This must be something that the packager is doing that haul isn't.
 //   I also so people complaining about this in Jest as well.
 //
-try {
-  global.self = global; /* eslint-disable-line */
-} catch (e) {
-  console.log(e);
-}
+// try {
+//   global.self = global; /* eslint-disable-line */
+// } catch (e) {
+//   console.log(e);
+// }
 
 require('InitializeCore');
